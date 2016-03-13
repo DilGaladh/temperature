fe9ac12d2bc073432e9174e42f671bdce6a6b162
DilGaladh <florian.maurice@gmail.com>
2016-03-13 17:12:56 +0100

    first version of website

---
+++ routes/sondes.js
@@ -0,0 +1,89 @@
+// Load node modules
+var fs = require('fs');
+var util = require('util');
+var http = require('http');
+var express = require('express');
+var router = express.Router();
+
+var Engine = require('tingodb')();
+var db = new Engine.Db('./data/', {});
+
+// Read current temperature from sensor
+function readTemp(callback){
+   fs.readdir('/sys/bus/w1/devices/', function(err, files){
+	var sondesData = {
+			name: "test",
+			date: new Date(),
+			data: {
+				sonde1 : 0,
+				sonde2 : 0,
+				sonde3 : 0,
+				sonde4 : 0,
+			}
+
+		}
+	var cpt=1;
+	for(i in files){
+		if(files[i].match("28*")){
+			var buffer = fs.readFileSync('/sys/bus/w1/devices/'+files[i]+'/w1_slave');
+			{
+			      if(buffer != undefined){
+				      // Read data from file (using fast node ASCII encoding).
+				      var data = buffer.toString('ascii').split(" "); // Split by space
+				
+				      // Extract temperature from string and divide by 1000 to give celsius
+				      var temp  = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+				
+				      // Round to one decimal place
+				      temp = Math.round(temp * 10) / 10;
+				
+				      // Add date/time to temperature
+				      sondesData.data['sonde'+cpt] = temp;
+				}			
+			      
+	   		}
+			cpt++;
+		}
+	}
+	// Execute call back with data
+	callback(sondesData);
+   });
+   
+};
+
+function insertTemp(data){
+	var collection = db.collection('temperatures');
+	//console.log(collection);
+	//collection.insert({'a':1})
+	console.log("data:"+JSON.stringify(data));
+	collection.insert(data);
+	db.close();
+}
+
+// Create a wrapper function which we'll use specifically for logging
+function logTemp(interval){
+      // Call the readTemp function with the insertTemp function as output to get initial reading
+      readTemp(insertTemp);
+      // Set the repeat interval (milliseconds). Third argument is passed as callback function to first (i.e. readTemp(insertTemp)).
+      setInterval(readTemp, interval, insertTemp);
+};
+
+/* GET temperatures scan. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  
+  readTemp(function(data){
+    res.status(200).send(JSON.stringify(data));	
+  });
+  
+  //res.render('index', { title: 'Temperatures', content: collection, dd1: "var dd1 = [0,8,4,5,6];" });
+});
+
+module.exports = router;
+
+// Start temperature logging (every 1 min).
+var msecs = (60 * 1) * 1000; // log interval duration in milliseconds
+logTemp(msecs);
+// Send a message to console
+console.log('Server is logging to database at '+msecs+'ms intervals');
+
---
+++ routes/temperatures.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var assert = require('assert');
+var router = express.Router();
+
+// database
+
+var Engine = require('tingodb')();
+
+var db = new Engine.Db('./data/', {});
+
+// insertion test
+var insertDocument = function(db, callback) {
+	var collection = db.collection('temperatures');
+	//console.log(collection);
+	//collection.insert({'a':1});
+	collection.insert(
+		{
+			name: "test",
+			date: new Date(),
+			data: {
+				sonde1 : 20*Math.random(),
+				sonde2 : 20*Math.random(),
+				sonde3 : 20*Math.random(),
+				sonde4 : 20*Math.random(),
+			}
+
+		}, function(err, result){
+		assert.equal(err,null);
+		console.log("inserted a document into the temperatures collection.");
+		callback(result);
+	});
+};
+
+/*insertDocument(db, function(){
+	db.close();
+});*/
+
+/* GET temperatures listing. */
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  var collection = db.collection('temperatures');
+collection.createIndex({date: -1}, {background: true});
+  var series = 'var series = [';
+  var cursor = collection.find().sort({date:-1}).limit(660*24);
+	/*res.send(collection);*/
+	//console.log("test:"+test);
+        //console.log("collection:"+collection);
+	var docs = cursor.toArray(function(err, docs){
+		//console.log(JSON.stringify(docs));
+		//console.log("docs:"+docs);
+		docs.reverse();
+		var serie = new Object();
+		for(var cpt=0;cpt<4;cpt++){
+			serie[cpt] = "{name:'sonde"+(cpt+1)+"',data:[";
+			for(var doc in docs){
+				//console.log(doc+" doc:"+docs[doc]);
+				var dataArray = docs[doc].data;
+				//console.log("dataArray:"+dataArray);
+				var keys = Object.keys(dataArray);
+				serie[cpt] += "["+JSON.stringify(docs[doc].date.getTime())+","+JSON.stringify(dataArray[keys[cpt]])+"],";
+			}
+			serie[cpt] += "]},";
+		}
+		//console.log(serie);
+		// concatenate all
+		series+=serie[0]+serie[1]+serie[2]+serie[3];
+		series += ']';
+		//console.log("series:"+series);
+	  	res.render('index', { title: 'Temperatures', content: "salut", series: series ,dd1: "var dd1 = [0,8,4,5,6];" });
+	  	//console.log(docs);
+		console.log("docs size:"+docs.length);
+		db.close();
+	});
+		
+
+	
+  
+  //res.render('index', { title: 'Temperatures', content: collection, dd1: "var dd1 = [0,8,4,5,6];" });
+});
+
+module.exports = router;
