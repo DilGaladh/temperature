d363015f1b609d53542469ca039ffe4b52937511
florian maurice <florian.maurice@gmail.com>
2023-09-04 22:05:24 +0200

    new scripts

--- raspberry/get_from_github.sh
+++ raspberry/get_from_github.sh
@@ -1,8 +1,9 @@
+cd ~
 git clone https://github.com/DilGaladh/temperature.git
 cd temperature
 git pull --rebase
 git reset --hard origin/master
 npm install
 cd ..
-sudo sh relaunch_website.sh
+sudo sh temperature/raspberry/relaunch_website.sh
 
