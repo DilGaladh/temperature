26c870f1b194904ceb6feded2e3232328245b68d
florian maurice <florian.maurice@gmail.com>
2024-01-07 20:17:50 +0100

    change execution script to output progress

--- raspberry/get_from_github.sh
+++ raspberry/get_from_github.sh
@@ -1,8 +1,10 @@
 cd ~
+echo "get with git"
 git clone https://github.com/DilGaladh/temperature.git
 cd temperature
 git pull --rebase
 git reset --hard origin/master
+echo "install with npm"
 npm install --prefer-offline
 cd ..
 sudo sh temperature/raspberry/relaunch_website.sh
