d653c50e75581760d87b3530ca0f2c762dfa7fb1
florian maurice <florian.maurice@gmail.com>
2024-01-11 23:16:54 +0100

    tentative with absolute paths

--- raspberry/relaunch_website.sh
+++ raspberry/relaunch_website.sh
@@ -1,8 +1,10 @@
-cd temperature
+pwd
+cd ~/temperature
 echo "will kill the website if running"
-sudo killall node &
+sudo killall node
+pwd
+cd ~/temperature
 echo "will run the website again"
 # for debugging
 #sudo node --inspect ./bin/www
 sudo node ./bin/www
-echo "website is running"
