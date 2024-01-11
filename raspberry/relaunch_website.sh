1dc664d1f4fa752a3de45cd168b5022e697cb926
florian maurice <florian.maurice@gmail.com>
2024-01-11 23:53:45 +0100

    local

--- raspberry/relaunch_website.sh
+++ raspberry/relaunch_website.sh
@@ -1,10 +1,10 @@
 echo $PWD
-cd ~/temperature
+cd temperature
 echo "will kill the website if running"
 sudo killall node
 sleep 10
 echo $PWD
-cd ~/temperature
+cd temperature
 echo "will run the website again"
 # for debugging
 #sudo node --inspect ./bin/www
