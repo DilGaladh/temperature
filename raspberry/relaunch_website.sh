3e6b8cb8ac495a7f2652db2f30289587f3402f48
florian maurice <florian.maurice@gmail.com>
2024-01-11 22:48:17 +0100

    try to make sure server launches

--- raspberry/relaunch_website.sh
+++ raspberry/relaunch_website.sh
@@ -1,7 +1,8 @@
 cd temperature
 echo "will kill the website if running"
 sudo killall node
+sudo sleep 10
 echo "will run the website again"
 # for debugging
 #sudo node --inspect ./bin/www
-sudo node ./bin/www
+sudo node ./bin/www && echo "website is running"
