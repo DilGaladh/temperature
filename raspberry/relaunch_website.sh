1cdce5ed374a341239da924ae85bf0987cc0812f
florian maurice <florian.maurice@gmail.com>
2024-01-11 22:55:37 +0100

    website not launching anymore, trying to figure out why

--- raspberry/relaunch_website.sh
+++ raspberry/relaunch_website.sh
@@ -4,4 +4,5 @@ sudo killall node &
 echo "will run the website again"
 # for debugging
 #sudo node --inspect ./bin/www
-sudo node ./bin/www && echo "website is running"
+sudo node ./bin/www
+echo "website is running"
